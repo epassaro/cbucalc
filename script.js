98e8059ac750362346dfe134ca1dc49cf7befaca
Ezequiel Pássaro <epassaro15@gmail.com>
2021-12-29 23:32:42 -0300

    add html, css and js files

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict'
+
+var cbuSpan = document.getElementById("cbu");
+var cod_bcra = document.getElementById("cod_bcra");
+var nro_sucursal = document.getElementById("nro_sucursal");
+var nro_cuenta = document.getElementById("nro_cuenta");
+
+var span = a => {
+  return "<span>" + a + "</span>";
+};
+
+var calcular = () => {
+
+  let B = cod_bcra.value;
+  let S = nro_sucursal.value;
+  let C = nro_cuenta.value;
+
+  var verificador1 =
+      B[0] * 7 +
+      B[1] * 1 +
+      B[2] * 3 +
+      S[0] * 9 +
+      S[1] * 7 +
+      S[2] * 1 +
+      S[3] * 3;
+  verificador1 = (10 - verificador1 % 10) % 10;
+
+  var verificador2 =
+      C[0] * 3 +
+      C[1] * 9 +
+      C[2] * 7 +
+      C[3] * 1 +
+      C[4] * 3 +
+      C[5] * 9 +
+      C[6] * 7 +
+      C[7] * 1 +
+      C[8] * 3 +
+      C[9] * 9 +
+      C[10] * 7 +
+      C[11] * 1 +
+      C[12] * 3;
+  verificador2 = (10 - verificador2 % 10) % 10;
+
+  if (isNaN(verificador1) || isNaN(verificador2)) {
+    cbuSpan.innerHTML = span("Número incorrecto de dígitos");
+    cbuSpan.rawDigits = null
+  } else {
+    cbuSpan.rawDigits = B + S + verificador1 + C + verificador2;
+    cbuSpan.innerHTML = "CBU: " + B + S + span(verificador1) + C + span(verificador2);
+  }
+}
+
+var generar = () => {
+  calcular();
+}
+
+generar();
